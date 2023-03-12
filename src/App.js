67c225e4d42a04e5da57743d7e9bc31a290cef5f
sivajithms <sivajithm@gmail.com>
2023-03-12 23:18:05 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState } from 'react';
 import './App.css';
 
+
+
 function App() {
+  const [toDos, setToDos] = useState([])
+  const [toDo, setToDo] = useState('')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input type="text" onChange={(e) => setToDo(e.target.value)} placeholder="🖊️ Add item..." />
+        <i onClick={() => setToDos([...toDos,{id:Date.now() , text: toDo, status:false }])} className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+        {
+          
+          toDos.map((value) => {
+            return (
+              <div className="todo">
+                <div className="left">
+                  <input onChange={(e)=>{
+                    console.log(e.target.checked);
+                    setToDos(toDos.filter(obj=>{
+                      if(value.id===obj.id){
+                        obj.status=e.target.checked
+                      }
+                      return true
+                    }))
+                  }} type="checkbox" name="" id="" />
+                  <p>{value.text}</p>
+                </div>
+                <div className="right">
+                  <i className="fas fa-times"  onClick={()=>{
+                    setToDos(toDos.filter(obj=>{
+                      return(value.id!==obj.id)                     
+                      
+                    }))
+                  }}></i>
+                </div>
+              </div>
+            )
+          })
+        }
+
+        {
+          toDos.map((obj)=>{
+            if(obj.status){
+              return(
+                <div>
+                  <br/>
+                  <br/>
+                  <br/>
+                  <h1>Finished works</h1>
+                  <h2>{obj.text}</h2>
+                </div>
+              )
+            }
+            return true
+          })
+        }
+      </div>
     </div>
   );
 }
